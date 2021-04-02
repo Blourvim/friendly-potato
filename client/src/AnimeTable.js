e599957b425914a260f36381f566c2a22b5dcc7d
Dorian <silisyumtr@gmail.com>
2021-04-02 19:37:23 +0300

    style the table

--- client/src/AnimeTable.js
+++ client/src/AnimeTable.js
@@ -1,35 +1,38 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-
+import './style.css';
 
 
 const AnimeTable =()=>{
   const [items,setItems] = useState();
-
+const removeItem =()=>{
+console.log("this.image_url")
+}
   const fetchOnLoad = async()=>{
     const response = await axios.get("https://api.jikan.moe/v3/top/anime/1/upcoming")
     setItems(Object.entries(response.data.top))
   }
   fetchOnLoad()
 
-
-
-
 return(    
   <>
+  <div className="anime-block-wrapper">
 {
   items&&
   items.map((item,)=>{
-    const {mal_id, image_url,title} = item[1]
-
+    const {mal_id,image_url,title} = item[1]
     return(
-      <div>{mal_id}
-        {title}
+          
 
-      <img src={image_url} alt={title}/>
+      <figure key={mal_id} className="anime-block">
+        
+
+      <img onClick={()=>removeItem()}className="anime-image"src={image_url} alt={title}/>
+      <figcaption className="anime-title">{title}</figcaption>
+
       
-      </div>
+      </figure>
       
       )
   })
@@ -37,8 +40,7 @@ return(
 
   }
 
-  <button>Fetch Data</button>
-"hello world"
+</div>
   </> )
 }
 
