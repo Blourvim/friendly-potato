e954ede72bc1ed21a4bf62afa5ffb25fc60e53d3
Dorian <silisyumtr@gmail.com>
2021-08-09 06:31:17 +0300

    add anime card and update dependencies

---
+++ client/src/AnimeCard.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Card,CardActionArea,Typography,CardMedia,CardContent} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+
+    root:{
+      marginTop:'2rem',
+        width:'345px',
+        borderRadius:'15px',
+        paddingTop:'40px',
+        marginBottom:'40px',
+        backgroundColor:'#9bc8dc'
+      },
+    content:{
+      paddingBottom:'30px',
+      backgroundColor:'#9bc8dc',
+    color:'#36454f',
+
+
+    },
+    media:{
+      width:'auto',
+      padingTop:'30px'
+    }
+
+
+})
+
+
+const AnimeCard =(props)=>{
+
+    const classes = useStyles()
+console.log(props)
+const {mal_id,image_url,title,url,synopsis} = props.info
+return(
+    <Card className={classes.root}>
+      <a href={url} rel='noreferrer'target='_blank'>
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        alt={title}
+        height="auto"
+        image={image_url}
+        title={title}
+        className={classes.media}
+      />
+    </CardActionArea>
+    </a>
+      <CardContent className={classes.content}>
+        <Typography gutterBottom variant="h5" component="h2">
+        {title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {synopsis}
+        </Typography>
+      </CardContent>
+
+  </Card>
+)
+
+}
+
+export default AnimeCard
